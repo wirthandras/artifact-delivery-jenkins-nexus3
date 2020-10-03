1830323e786cec0a23b0b3bc4d2f44b96619d13f
wirthandras <agrippa.wia@gmail.com>
2020-10-03 21:33:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-sudo apt -y install curl
-sudo snap install docker
+# Install packages to allow apt to use a repository over HTTPS
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Set up the repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# Update source lists
+sudo apt-get update
+# Install Docker
+sudo apt-get install docker-ce
 
 #install docker compose (https://docs.docker.com/compose/install/)
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
