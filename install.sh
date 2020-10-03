a2936dae455c1359306f4ff7be676bf0e3d7f459
Andras Wirth <agrippa.wia@gmail.com>
2020-10-03 09:11:49 -0700

    install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt install curl
+sudo apt install git
+sudo apt install docker
+
+#install docker compose (https://docs.docker.com/compose/install/)
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+
+sudo chmod +x ./start.sh
