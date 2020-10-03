1c2b98f8ea389f4b56a914f67a3783c49f9dc5b7
Andras Wirth <agrippa.wia@gmail.com>
2020-10-03 05:46:45 -0700

    Docker compose build jenkins image locally, Jenkins plugins and dotnet core support

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-if [[ "$(docker images -q wajenkins:1.0 2> /dev/null)" == "" ]]; then
-  sudo docker build -t wajenkins:1.0 .
-fi
-
 sudo chmod a+rw /var/run/docker.sock
-docker-compose up -d
+docker-compose up -d --build
